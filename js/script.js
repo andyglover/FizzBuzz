3ccfe6ba547fbe3727edd76e55b4d54ace6f5989
Andy Glover <andrew.l.glover@gmail.com>
2022-07-17 16:44:14 -0700

    Add boilerplate, js that counts to 100

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// FizzBuzz rules:
+// Print numbers from 1 to 100
+// If the number is divisible by 3, print "Fizz".
+// If the number is divisible by 5 print "Buzz".
+// If the number is divisible by 3 and 5, print "FizzBuzz".
+// Otherwise, print the number.
+
+let number = 1;
+for(i=0;i<100;i++){
+    console.log(number);
+    number++;
+}
