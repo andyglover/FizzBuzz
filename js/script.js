edd5667d13f8b6b1fcb15a0e24f178b7ef715529
Andy Glover <andrew.l.glover@gmail.com>
2022-07-17 16:47:01 -0700

    Add structure of if statements to js file

--- js/script.js
+++ js/script.js
@@ -7,6 +7,22 @@
 
 let number = 1;
 for(i=0;i<100;i++){
-    console.log(number);
+    // If the number is divisible by 3, print "Fizz".
+    if(true){
+        console.log(number);
+    }
+    // If the number is divisible by 5 print "Buzz".
+    else if(true){
+        console.log(number);
+    }
+    // If the number is divisible by 3 and 5, print "FizzBuzz".
+    else if(true){
+        console.log(number);
+    }
+    // Otherwise, print the number.
+    else{
+        console.log(number);
+    }
+
     number++;
 }
